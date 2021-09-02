7b3c3ba61973959f6bab71906148970f53d9b292
SergeyAN <lookbugs@mail.ru>
2021-09-03 01:46:25 +0300

    change all tests

--- src/test/java/com/demoqa/PracticeFormTests.java
+++ src/test/java/com/demoqa/PracticeFormTests.java
@@ -15,98 +15,73 @@ public class PracticeFormTests {
 
     @BeforeAll
     static void setup() {
-        Configuration.baseUrl = "https://demoqa.com/automation-practice-form";
-
+        Configuration.browser = "firefox";
+        Configuration.baseUrl = "https://demoqa.com";
         Configuration.startMaximized = true;
     }
 
-
     @Test
     void nameLastNameTest() {
-        //open("/automation-practice-form");
+        String path = "D:\\Download\\test.pdf";
+        File file = new File(path);
+
+        open("/automation-practice-form");
+
+        // nameLastNameTest
         $("input#firstName").setValue("TestName");
         $("input#lastName").setValue("TestLastName");
-    }
 
-    @Test
-    void emailTest() {
-        open("/automation-practice-form");
+        // emailTest
         $("input#userEmail").setValue("test@gmail.com");
-    }
 
-    @Test
-    void GenderRadioButtonTest() {
-        open("/automation-practice-form");
+        // GenderRadioButtonTest
         $("#genterWrapper").$(byText("Male")).click();
         $("#genterWrapper").$(byText("Female")).click();
         $("#genterWrapper").$(byText("Other")).click();
-
         $("#genterWrapper").$(byText("Other")).isSelected();
-    }
 
-    @Test
-    void mobilePhoneTest() {
-        open("/automation-practice-form");
+        // mobilePhoneTest
         $("input#userNumber").setValue("5659874563");
 
-        $("input#userNumber").shouldHave(value("5659874563"));
-    }
-
-    @Test
-    void dateOfBirthTest() {
-        open("/automation-practice-form");
+        // dateOfBirthTest
         $("#dateOfBirthInput").click();
-
         $(".react-datepicker__year-dropdown-container--select").click();
         $(".react-datepicker__year-select").$(byText("2000")).click();
-
         $(".react-datepicker__month-dropdown-container--select").click();
         $(".react-datepicker__month-select").$(byText("July")).click();
-
         $(".react-datepicker__month .react-datepicker__week .react-datepicker__day--013").click();
-    }
 
-    @Test
-    void subjectsFieldTest() {
-        open("/automation-practice-form");
+        // subjectsFieldTest
         $("#subjectsContainer").click();
         $("#subjectsInput").setValue("Hindi").pressEnter();
         $("#subjectsInput").setValue("Math").pressEnter();
-    }
 
-    @Test
-    void HobbiesCheckBoxesTest() {
-        open("/automation-practice-form");
+        // HobbiesCheckBoxesTest
         $("#hobbiesWrapper").$(byText("Sports")).click();
         $("#hobbiesWrapper").$(byText("Reading")).click();
         $("#hobbiesWrapper").$(byText("Music")).click();
         $("#hobbiesWrapper").$(byText("Reading")).click();
         $("#hobbiesWrapper").$(byText("Reading")).click();
-    }
 
-    @Test
-    void selectPictureTest() {
-        String path = "D:\\Download\\test.pdf";
-        File file =new File(path);
-
-        open("/automation-practice-form");
+        // selectPictureTest
         $("input#uploadPicture").uploadFile(file);
+
+        // CurrentAddressTest
+        $("#currentAddress-wrapper textarea#currentAddress").setValue("Moskow, Lenina 14, a.3").pressEnter();
+
+        // selectStateAndselectCityTest
+        $("#state").click();
+        $("#state").$(byText("Haryana")).click();
+        $("#city").click();
+        $("#city").$(byText("Karnal")).click();
+
+        // submitTest
+        $("button#submit").pressEnter();
+
+        // todo
+        //$("input#firstName").shouldHave(value("TestName"));
+        //$("input#lastName").shouldHave(value("TestLastName"));
+        //$("input#userEmail").shouldHave(value("test@gmail.com"));
+        //$("input#userNumber").shouldHave(value("5659874563"));
     }
-
-    // ! не работает, хз
-    @Test
-    void CurrentAddressTest() {
-        open("/automation-practice-form");
-        $("#currentAddress-wrapper textarea#currentAddress").setValue("Moskow, Lenina 14, a.3");
-
-    }
-
-    @Test
-    void selectStateTest() {
-        open("/automation-practice-form");
-        $("#state").$(byText("Select State")).click();
-
-    }
-
-
 }
